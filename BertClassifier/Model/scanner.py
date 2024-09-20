2ee89c24b1bf9f527580731b65955eccc63b7877
badwoman <3242204470@qq.com>
2024-09-20 23:06:42 +0800

    update controller: get ImgData

--- BertClassifier/Model/scanner.py
+++ BertClassifier/Model/scanner.py
@@ -1,5 +1,6 @@
 import torch
 from transformers import BertForSequenceClassification, BertTokenizer
+from paddleocr import PaddleOCR
 
 model_url = './trained_model'
 
@@ -23,3 +24,26 @@ class TextScanner:
             category = self.predict(text=text)
             result_map[text] = category
         return result_map
+
+class ImgScanner:
+    def __init__(self) -> None:
+        self.ocr = PaddleOCR(use_angle_cls=True, lang="ch")
+
+    def extract(self, path) -> list:
+        result = self.ocr.ocr(img=path, cls=True)
+        extracted_text = []
+        if len(result) > 0:
+            for idx in range(len(result)):
+                res = result[idx]
+                if res:
+                    for line in res:
+                        textData, confidence = line[1]
+                        print(textData, confidence)
+                        if confidence > 0.95:
+                            extracted_text.append(textData)
+                else:
+                    print("can not recognize")
+            if not extracted_text:
+                print("No text recognized with sufficient confidence.")
+
+        return extracted_text
--- BertClassifier/contorller.py
+++ BertClassifier/contorller.py
@@ -1,33 +1,58 @@
-from flask import Flask,request,jsonify
+from flask import Flask, request, jsonify
 from flask_cors import CORS
-from Model.scanner import TextScanner
+from Model.scanner import TextScanner, ImgScanner
+import requests
+import shutil
+import os
 
 page_text = []
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/')
-@app.route('/App',methods = ['POST'])
+
+@app.route("/")
+@app.route("/App", methods=["POST"])
 def getTextData():
     global page_text
-    data = request.json.get('data', [])
+    img_folder = "./img"
+    if os.path.exists(img_folder):
+        shutil.rmtree(img_folder)
+    os.makedirs(img_folder)
+    data = request.json.get("TextData", [])
+    img_data = request.json.get("ImgData", [])
     page_text.clear()
     page_text = data
-    print("Received data:", data)
-    return jsonify({'status':0,
-                    'message':'success',
-                    'data':'success'})
+    for index, url in enumerate(img_data):
+        try:
+            response = requests.get(url=url)
+            response.raise_for_status()
 
-@app.route('/')
-@app.route('/get',methods = ['GET'])
+            img_path = os.path.join(img_folder, f"img_{index+1}.jpg")
+            with open(img_path, "w") as img_file:
+                img_file.write(response.content)
+        except requests.exceptions.RequestException as e:
+            print(f"failed to download image {index + 1 }:{e}")
+    OCRmodel = ImgScanner()
+    for filename in os.listdir(img_folder):
+        file_path = os.path.join(img_folder, filename)
+
+        img_text = OCRmodel.extract(path=filename)
+        if img_text:
+            page_text.append(img_text)
+
+    print("Received data:", data)
+    return jsonify({"status": 0, "message": "success", "data": "success"})
+
+
+@app.route("/")
+@app.route("/get", methods=["GET"])
 def getResponse():
     global page_text
     model = TextScanner()
     result = model.acting(page_text)
-    return jsonify({'status':0,
-                    'message':'success',
-                    'data':result})
+    return jsonify({"status": 0, "message": "success", "data": result})
 
-if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8080)
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=8080)
